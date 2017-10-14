2e80b2fbc0233040fd6c382a479ea64a97ea6c0a
Nullerz5540 <32801574+Nullerz5540@users.noreply.github.com>
2017-10-14 19:59:15 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * Discord
+ * Version 1.2.0
+ * Nullerz
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
